921c2aebdd7d9d01189bda7952263fce8a90b3c0
Santiago Cuello <sjcuello@gmail.com>
2024-02-21 11:44:50 -0300

    Simplify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Button, Grid } from '@mui/material';
-import TableMUI from './components/Table';
-import DataGrid from './components/DataGrid';
-import TabsMUI from './components/Tab';
-import { TabContent } from './interfaces';
 import ThemeContainer from './components/ThemeContainer';
-
-const tabArray: TabContent[] = [
-  {
-    title: '🎹 Buttons',
-    children: (
-      <Grid container spacing={2}>
-        <Grid xs={12} spacing={2}>
-          <Button sx={{ margin: '1rem' }} variant="text">
-            Primary
-          </Button>
-          <Button sx={{ margin: '1rem' }} variant="contained">
-            Primary
-          </Button>
-          <Button sx={{ margin: '1rem' }} disabled={true} variant="contained">
-            Primary
-          </Button>
-          <Button sx={{ margin: '1rem' }} variant="outlined">
-            Primary
-          </Button>
-          <Button sx={{ margin: '1rem' }} disabled={true} variant="outlined">
-            Primary
-          </Button>
-        </Grid>
-        <Grid xs={12}>
-          <Button sx={{ margin: '1rem' }} variant="contained" color="secondary">
-            Secondary
-          </Button>
-          <Button
-            sx={{ margin: '1rem' }}
-            disabled={true}
-            variant="contained"
-            color="secondary">
-            Secondary
-          </Button>
-        </Grid>
-        <Grid xs={12}>
-          <Button sx={{ margin: '1rem' }} variant="contained" color="tertiary">
-            Tertiary
-          </Button>
-          <Button
-            sx={{ margin: '1rem' }}
-            disabled={true}
-            variant="contained"
-            color="tertiary">
-            Tertiary
-          </Button>
-        </Grid>
-      </Grid>
-    ),
-  },
-  { title: '📦 Simple Table', children: <TableMUI /> },
-  { title: '⚡️ Data Grid', children: <DataGrid /> },
-];
+import { Typography } from '@mui/material';
 
 const App = () => (
   <ThemeContainer>
     <div className="container">
-      <TabsMUI tabArray={tabArray} />
+      <Typography variant="h2" gutterBottom align="center">
+        It's just a playgorund for MUI components or any other components
+      </Typography>
     </div>
   </ThemeContainer>
 );
